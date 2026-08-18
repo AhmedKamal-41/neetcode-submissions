674aefa083d56caadaa9760840af93c1812316fc
Ahmed Ali <ahmed.ali12@qmail.cuny.edu>
2026-08-17 22:43:00 -0400

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,31 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.nums = []
+
+    def put(self, key: int, value: int) -> None:
+        for pair in self.nums:
+            if pair[0] == key:
+                pair[1] = value
+                return
+
+        self.nums.append([key, value])
+
+    def get(self, key: int) -> int:
+        for pair in self.nums:
+            if pair[0] == key:
+                return pair[1]
+
+        return -1
+
+    def remove(self, key: int) -> None:
+        for pair in self.nums:
+            if pair[0] == key:
+                self.nums.remove(pair)
+                return
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
